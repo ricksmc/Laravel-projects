9e71399e4d0c2c4ae84f84af8a1fddacf96d7c54
Ricardo Santos Melo Cunha <ricardosmc@aol.com>
2021-12-08 21:35:08 -0300

    Alterated Project
    Alterated project

--- sisklin/routes/web.php
+++ sisklin/routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('loginPage');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
